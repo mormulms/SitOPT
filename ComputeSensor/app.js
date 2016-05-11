a58a247d78a03dc349f96ec6a4ef32cc34e50115
Pascal Hirmer <pascalhirmer@googlemail.com>
2016-05-11 15:51:11 +0200

    replaced app.js to enable use case

--- ComputeSensor/app.js
+++ ComputeSensor/app.js
@@ -3,29 +3,21 @@ var os  = require('os-utils');
 var express = require('express');
 var app = express();
 
-app.get('/rmp/sensordata/:objectId/:sensorId', function(req, res) {
-    var sensor = req.params.sensorId;
-    var objectId = req.params.objectId;
-    res.type('text/plain');
-    switch (sensor) {
-        case 'memorySensor':
-            var memoryUsage = os.freemem();
-            memoryUsage /= os.totalmem();
-            res.send('{"sensor":"' +sensor + '","objectId":"' + objectId + '", "value":"9"}');
-            break;
-
-        case 'cpuSensor':
-            os.cpuFree(function(value) {
-                res.send('{"sensor":"' +sensor + '","objectId":"' + objectId + '", "value":"30"}');
-            });
-            break;
-
-        case 'watchdogSensor':
-            res.send('{"sensor":"' +sensor + '","objectId":"' + objectId + '", "value":"200"}');
-            break;
-    }
+app.get('/rmp/sensordata/PC/memorySensor', function(req, res) {
+  res.type('text/plain');
+  os.cpuFree(function(value) {
+  
+	var memoryUsage = '{"payload": {\"timestamp\": ' + new Date().getTime() +', \"value\":' + '\"' + Math.round(12000 - os.freemem()).toString() + '\"}}';
+	res.send(memoryUsage);
+ });
 });
 
-app.listen(8080, function() {
-    console.log('IT LIVES')
+app.get('/rmp/sensordata/PC/cpuSensor', function(req, res) {
+  res.type('text/plain');
+  os.cpuFree(function(value) {
+    var cpuUsage = '{"payload": {\"timestamp\": ' + new Date().getTime() +', \"value\":' + '\"' + Math.round(((1-value)*100).toString()) + '\"}}';
+	res.send(cpuUsage);
+ });
 });
+
+app.listen(process.env.PORT || 1337);
