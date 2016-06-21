2cdd4b6a9497b4ab27d6f26e7ee6d0bc469bd074
Armin Hueneburg <hueneburg.armin@gmail.com>
2016-06-21 20:57:01 +0200

    changed dbnames to plural
    db names were singular in db_setup.sh

--- db_setup.sh
+++ db_setup.sh
@@ -2,21 +2,21 @@
 
 COUCH=$1
 
-curl -X PUT http://$(COUCH):5984/Sensor
-curl -X PUT http://$(COUCH):5984/Agent
-curl -X PUT http://$(COUCH):5984/Thing
-curl -X PUT http://$(COUCH):5984/Situationstemplate
-curl -X PUT http://$(COUCH):5984/Situation
-curl -X PUT http://$(COUCH):5984/Actuator
+curl -X PUT http://${COUCH}:5984/Sensor
+curl -X PUT http://${COUCH}:5984/Agent
+curl -X PUT http://${COUCH}:5984/Thing
+curl -X PUT http://${COUCH}:5984/Situationstemplate
+curl -X PUT http://${COUCH}:5984/Situation
+curl -X PUT http://${COUCH}:5984/Actuator
 
 MONGO=$2
 
-mongo SitDB --host $(MONGO) --eval 'db.createCollection("Sensor")'
-mongo SitDB --host $(MONGO) --eval 'db.createCollection("Agent")'
-mongo SitDB --host $(MONGO) --eval 'db.createCollection("Thing")'
-mongo SitDB --host $(MONGO) --eval 'db.createCollection("Situationstemplate")'
-mongo SitDB --host $(MONGO) --eval 'db.createCollection("Situation")'
-mongo SitDB --host $(MONGO) --eval 'db.createCollection("Actuator")'
+mongo SitDB --host ${MONGO} --eval 'db.createCollection("Sensors")'
+mongo SitDB --host ${MONGO} --eval 'db.createCollection("Agents")'
+mongo SitDB --host ${MONGO} --eval 'db.createCollection("Things")'
+mongo SitDB --host ${MONGO} --eval 'db.createCollection("Situationstemplates")'
+mongo SitDB --host ${MONGO} --eval 'db.createCollection("Situations")'
+mongo SitDB --host ${MONGO} --eval 'db.createCollection("Actuators")'
 
-mongo RBS --host $(MONGO) --eval 'db.createCollection("caches")'
-mongo RBS --host $(MONGO) --eval 'db.createCollection("sensors")'
+mongo RBS --host ${MONGO} --eval 'db.createCollection("caches")'
+mongo RBS --host ${MONGO} --eval 'db.createCollection("sensors")'
