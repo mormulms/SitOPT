bf18b17b77c7bb67695db70c7f20a3821ab80fdf
mormul <mormulms@ipvs.uni-stuttgart.de>
2017-01-12 08:25:44 +0100

    added comments to /RMP/initializers/database.js

--- RMP/initializers/database.js
+++ RMP/initializers/database.js
@@ -6,14 +6,19 @@ module.exports = {
         next();
     },
     start: function(api, next){
+
+    	//Setup mongoose for MongoDB database access (RBS - from config file /RMP/config/database.config.js)
         var mongoose = require('mongoose');
         var config = require('../config/database.config.js');
         var url = "mongodb://" + config.user + ":" + config.password + "@" + config.host + ":" + config.port + "/" + config.name;
         mongoose.connect(url);
         api.database = mongoose.connection;
+
         api.database.on('error', function(error) {
             console.log(error);
         });
+
+        //Successful connection -> Creation of mongoose schema (obligatory) for sensor data 
         api.database.once('open', function(callback) {
             var cacheSchema = new mongoose.Schema({
                 objectID: String,
@@ -24,8 +29,10 @@ module.exports = {
                 sensorQuality: Number
             });
 
+           	//RBS contains "table" sensorCache that uses schema "cacheSchema"
             api.sensorCache = mongoose.model('Cache', cacheSchema);
 
+            // Creation of mongoose schema (obligatory) for sensors
             var sensorSchema = new mongoose.Schema({
                 objectID: String,
                 sensorID: String,
@@ -39,6 +46,8 @@ module.exports = {
                 defaultValueActive: { type: Boolean, default: false}
             });
 
+            //RBS contains "table" sensor that uses schema "sensorSchema"
+            //Definition of "api.sensor"
             api.sensor = mongoose.model('Sensor', sensorSchema);
         });
         next();
